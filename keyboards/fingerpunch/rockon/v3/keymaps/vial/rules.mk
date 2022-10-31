71c63c24dd025c5af7f7e80010c605de6e213ae5
Sadek Baroudi <sadekbaroudi@gmail.com>
2022-10-31 15:30:29 -0700

    one last error for github build action failing for rock on v3

--- keyboards/fingerpunch/rockon/v3/keymaps/vial/rules.mk
+++ keyboards/fingerpunch/rockon/v3/keymaps/vial/rules.mk
@@ -5,7 +5,6 @@ VIAL_ENCODERS_ENABLE = no
 ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
    VIALRGB_ENABLE := yes  # not required, but enabling for mouse button keys
 endif
-VIALRGB_ENABLE = yes # this should be set in the feature specific rules.mk files
 LTO_ENABLE = yes
 QMK_SETTINGS = no
 
