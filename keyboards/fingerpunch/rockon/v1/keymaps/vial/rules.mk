b04a333a0db7a90d9c4244c9109c28b9f4a663f4
Sadek Baroudi <sadekbaroudi@gmail.com>
2022-11-11 00:23:23 -0800

    reduce rock on v1 firmware for vial to fit on elite-c

--- keyboards/fingerpunch/rockon/v1/keymaps/vial/rules.mk
+++ keyboards/fingerpunch/rockon/v1/keymaps/vial/rules.mk
@@ -8,9 +8,4 @@ endif
 LTO_ENABLE = yes
 QMK_SETTINGS = no
 TAP_DANCE_ENABLE = no
-CASEMODES_ENABLE = yes
 
-ifeq ($(strip $(CASEMODES_ENABLE)), yes)
-	OPT_DEFS += -DCASEMODES_ENABLE
-    SRC += users/sadekbaroudi/casemodes.c
-endif
