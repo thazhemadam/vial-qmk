c0e46eedd39a7f7953d37ebe6ceebb8c7817794e
Sadek Baroudi <sadekbaroudi@gmail.com>
2023-08-10 22:32:17 -0700

    added ffkb lite to vial

---
+++ keyboards/fingerpunch/ffkb/lite/v1/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0x1E, 0x5F, 0xE2, 0x2F, 0x31, 0x0A, 0xAA, 0x47}
+
+#ifndef VIAL_INSECURE
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 2 }
+#endif
+                  
+// For via/vial only
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
