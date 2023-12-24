83f32fb78bc79e4cf31e285d384c8e9fd5642f4c
Sadek Baroudi <sadekbaroudi@gmail.com>
2023-12-23 16:52:51 -0800

    first attempt at vial tenbit firmware

--- bin/sync_qmk_to_vial.sh
+++ bin/sync_qmk_to_vial.sh
@@ -10,15 +10,16 @@ rm -rf keyboards/fingerpunch/bgkeeb/
 rm -rf keyboards/fingerpunch/bigbarobord/
 rm -rf keyboards/fingerpunch/euclid36/
 rm -rf keyboards/fingerpunch/euclid36proto/
+rm -rf keyboards/fingerpunch/dweeeeep/
 rm -rf keyboards/fingerpunch/ffkb/byomcu/v3/kle-via.json
 rm -rf keyboards/fingerpunch/ffkb/rp/
 rm -rf keyboards/fingerpunch/fpm101/
+rm -rf keyboards/fingerpunch/kucheza/
 rm -rf keyboards/fingerpunch/luakeeb/
 rm -rf keyboards/fingerpunch/pinkiesout/v2_ext/
 rm -rf keyboards/fingerpunch/stemcell_jumper_tester/
 rm -rf keyboards/fingerpunch/personal/
 rm -rf keyboards/fingerpunch/svlinky/
-rm -rf keyboards/fingerpunch/tenbit/
 rm -rf keyboards/fingerpunch/vulpes_majora/
 rm -rf keyboards/fingerpunch/vulpes_minora/
 rm -rf keyboards/fingerpunch/xivik/
---
+++ keyboards/fingerpunch/tenbit/5x12/keymaps/vial/config.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0xA4, 0xED, 0xA4, 0xB8, 0xC4, 0x0C, 0x69, 0x6C}
+
+#ifndef VIAL_INSECURE
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+#endif
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+
+#define TAP_CODE_DELAY 25
+
+#define TAPPING_FORCE_HOLD
+
