5054efbad22737c965b007f20916135067484389
Sadek Baroudi <sadekbaroudi@gmail.com>
2023-12-24 10:43:50 -0800

    added QMK settings flag to tenbit, by request

--- keyboards/fingerpunch/tenbit/5x12/keymaps/vial/rules.mk
+++ keyboards/fingerpunch/tenbit/5x12/keymaps/vial/rules.mk
@@ -5,5 +5,5 @@ ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
    VIALRGB_ENABLE := yes  # not required, but enabling for mouse button keys
 endif
 LTO_ENABLE = yes
-QMK_SETTINGS = no
+QMK_SETTINGS = yes
 
