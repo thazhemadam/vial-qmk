4b55f35b5a9a7e604bfcae488bd0b5e4ebf31e85
Sadek Baroudi <sadekbaroudi@gmail.com>
2024-04-09 19:25:42 -0700

    default right thumb to pointer dpi

--- keyboards/fingerpunch/ximi/keymaps/vial/keymap.c
+++ keyboards/fingerpunch/ximi/keymaps/vial/keymap.c
@@ -42,17 +42,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *        | MUTE |         | LOWER| Enter|  Del |    |BckSpc| Space| RAISE|          | MUTE |
  *        `------'         `--------------------'    `--------------------.          `------'
  *                         ,--------------------.    ,--------------------.
- *                         |Mouse1|Mouse3|Mouse2|    | Vol- | Mute | Vol+ |    // 3 way thumb switch
+ *                         |Mouse1|Mouse3|Mouse2|    | DPIU | DPIR | DPID |    // 3 way thumb switch
  *                         `--------------------'    `--------------------.
  */
 
 // Default config uses home row mods. So hold each of the keys on the home row to use ctrl, gui, alt, or shift
 [_QWERTY] = LAYOUT_ximi(
-  KC_ESC,       KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,            KC_BSLS,
-  KC_TAB,       LCTL_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D), LSFT_T(KC_F), KC_G,         KC_H,    RSFT_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,
-  KC_LSFT,      KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RCTL,
-                KC_MUTE,                    LOWER,        KC_ENT,       KC_DEL,       KC_BSPC, KC_SPC,       RAISE,                      KC_MUTE,
-                                            KC_MS_BTN1,   KC_MS_BTN3,   KC_MS_BTN2,   KC_VOLD, KC_MUTE,      KC_VOLU
+  KC_ESC,       KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,            KC_U,                    KC_I,         KC_O,         KC_P,            KC_BSLS,
+  KC_TAB,       LCTL_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D), LSFT_T(KC_F), KC_G,         KC_H,            RSFT_T(KC_J),            RALT_T(KC_K), RGUI_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,
+  KC_LSFT,      KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,            KC_M,                    KC_COMM,      KC_DOT,       KC_SLSH,         KC_RCTL,
+                KC_MUTE,                    LOWER,        KC_ENT,       KC_DEL,       KC_BSPC,         KC_SPC,                  RAISE,                      KC_MUTE,
+                                            KC_MS_BTN1,   KC_MS_BTN3,   KC_MS_BTN2,   FP_POINT_DPI_UP, FP_POINT_DPI_RESET,      FP_POINT_DPI_DN
 ),
 
 /* Raise
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                         `--------------------'    `--------------------.
  */
 [_ADJUST] =  LAYOUT_ximi(
-  _______,      RGB_TOG, RGB_RMOD, RGB_MOD, _______, _______,       KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   _______,
+  _______,      RGB_TOG, RGB_RMOD, RGB_MOD, _______, _______,           KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   _______,
   _______,      RGB_SPI, RGB_HUI,  RGB_SAI, RGB_VAI, _______,           KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  _______,
   _______,      RGB_SPD, RGB_HUD,  RGB_SAD, RGB_VAD, _______,           KC_F11,  KC_F12,  _______, QK_RBT,   QK_BOOT, _______,
                 _______,           _______, _______, _______,           _______, _______, _______,          _______,
