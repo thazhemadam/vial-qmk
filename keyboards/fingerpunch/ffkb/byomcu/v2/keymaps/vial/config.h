fc965d7bdaf613b02df1fb5348105416098279bc
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2024-08-06 02:45:11 +0530

    ffkb-v2(vial): swap default vial keymap with personal draft 5col keymap

--- keyboards/fingerpunch/ffkb/byomcu/v2/keymaps/vial/config.h
+++ keyboards/fingerpunch/ffkb/byomcu/v2/keymaps/vial/config.h
@@ -10,11 +10,28 @@
                                KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,     KC_TRNS,            KC_TRNS, \
                                KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,     KC_TRNS,            KC_TRNS }
 #endif
-
 #ifndef VIAL_INSECURE
 #define VIAL_UNLOCK_COMBO_ROWS { 3, 3 }
 #define VIAL_UNLOCK_COMBO_COLS { 3, 4 }
 #endif
-
 // For via/vial only
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+#define XXX KC_NO
+
+/*
+ * 5-col wrapper for the layout mechanism
+ */
+#define LAYOUT_ffkb_5col( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     K30, K31, K32, K33, K34,      K35, K36, K37, K38   \
+) \
+LAYOUT_ffkb( \
+XXX, K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, XXX, \
+XXX, K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, XXX, \
+XXX, K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, XXX, \
+          K30, K31, K32, K33,      K34, K35, K36, K37,              \
+                              K38 \
+)
